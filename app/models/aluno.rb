91a1646639c2b2c25f119117c0c1f7664fe5efca
William Alex <william.alexwasm@gmail.com>
2026-03-18 18:07:05 -0300

    nova chamada para setar o default do status depois de inicializar

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -2,9 +2,17 @@ class Aluno < ApplicationRecord
   validates :name, presence: true, uniqueness: {case_sensitive:false}
   validates :cpf, presence: true, uniqueness: true,numericality:true
   validates :data_nascimento, presence:true
-  validates :email, presence:true,confirmation:{case_sensitive:false},uniqueness:{case_sensitive:false}
-  validates :email_confirmation, presence:true, if: :email_changed?
+  validates :email, presence:true,confirmation:true,uniqueness:{case_sensitive:false}
+  validates :email_confirmation, presence:true
   validates :telefone,presence:true,numericality:true
   validates :gender,presence:true, format: {with: /\A[MFmf]\z/}
   validates :payment_method,presence:true, inclusion: {in:%w(Boleto Cartao Pix)}
-end
+
+  before_validation :downcase_email
+
+  def downcase_email
+    email_confirmation.try(:downcase!)
+    email.try(:downcase!)
+  end
+
+end 
--- app/models/fatura.rb
+++ app/models/fatura.rb
@@ -4,6 +4,8 @@ class Fatura < ApplicationRecord
   validates :valor_fatura, presence:true, numericality:true
   validates :status,presence:true,inclusion: {in: %w(Aberta Atrasada Paga)}
   
+  after_initialize :default_set, if: :new_record?
+
   private def default_set
         self.status = "Aberta"
   end
