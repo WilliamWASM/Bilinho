5772993523a87a19b38b07fb6ae42db800b17a16
William Alex <william.alexwasm@gmail.com>
2026-03-18 12:59:09 -0300

    adição de case_sensitive:false para nome e email e valor como número

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,10 +1,10 @@
 class Aluno < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: {case_sensitive:false}
   validates :cpf, presence: true, uniqueness: true,numericality:true
   validates :data_nascimento, presence:true
-  validates :email, presence:true,confirmation:true,uniqueness:true
+  validates :email, presence:true,confirmation:{case_sensitive:false},uniqueness:{case_sensitive:false}
   validates :email_confirmation, presence:true, if: :email_changed?
-  validates :telefone,presence:true
+  validates :telefone,presence:true,numericality:true
   validates :gender,presence:true, format: {with: /\A[MFmf]\z/}
   validates :payment_method,presence:true, inclusion: {in:%w(Boleto Cartao Pix)}
 end
